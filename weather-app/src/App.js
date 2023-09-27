7afca3b1f3c89b70499d32bab9145e0dc8ab7cee
heeyeon <dlgmldus3289@naver.com>
2023-09-27 19:48:14 +0900

    openweather api 호출

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  return <div className="App"></div>;
+  //위치 가져오기
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log("현재 위치: ", lat, lon);
+      callApi(lat, lon);
+    });
+  };
+  //api 호출
+  const callApi = async (lat, lon) => {
+    try {
+      const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      );
+
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  //앱 접속 시, 한 번만 요청
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
